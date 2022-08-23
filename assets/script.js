24de5e6cac7c7018760044606b7fc85b4d77bd2c
wevexOP <111082615+wevexOP@users.noreply.github.com>
2022-08-23 09:57:18 -0700

    Practicing more Arrays in my JS file for my prework-study-guide.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,10 @@
-var topic = "HTML";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
+for (var x = 0; x < topics.length; x++) {
+  console.log(topics[x]);
+ }
+
+/*
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -11,6 +16,8 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
+*/
+
 var shapes = ["triangle", "square", "pentagon", "circle"]
 for (var x = 0; x < shapes.length; x++) {
   console.log(shapes[x]);
