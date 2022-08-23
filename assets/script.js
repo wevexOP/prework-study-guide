1f31b0e70212c9404cd14b3576914018cfb2a65e
wevexOP <111082615+wevexOP@users.noreply.github.com>
2022-08-23 09:49:13 -0700

    Practicing Arrays in the JS file.

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,7 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
+var shapes = ["triangle", "square", "pentagon", "circle"]
+for (var x = 0; x < shapes.length; x++) {
+  console.log(shapes[x]);
+ }
